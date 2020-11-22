eb4cd10dba2f4c3348c43ed1c043ece7ac18ffb5
Tim Bass <neo@unix.com>
2020-11-22 16:17:59 +0700

    REFACTOR:  Change to constants to load plugin logic.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: container-names-with-gon
 # about: plugin to add container names or ids from yml to backup page
-# version: 0.0.9.51
+# version: 0.0.9.52
 # date: 22s Nov 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-container-names-with-gon
@@ -12,20 +12,18 @@ require "gon"
 
 Gon.global.test = "HELLO GON"
 
+app_root = "#{Rails.root}/plugins/discourse-container-names-with-gon/app"
 PLUGIN_NAME = "container-names-with-gon".freeze
+PLUGIN_LOGIC = "#{app_root}/lib/plugin_logic.rb"
 
 after_initialize do
-  app_root = "#{Rails.root}/plugins/discourse-container-names-with-gon/app"
-  plugin_logic = "#{app_root}/lib/plugin_logic.rb"
-  load File.open(plugin_logic)
+  load File.open(PLUGIN_LOGIC)
 
   Admin::AdminController.class_eval do
     before_action :do_info
 
     def do_info
-      app_root = "#{Rails.root}/plugins/discourse-container-names-with-gon/app"
-      plugin_logic = "#{app_root}/lib/plugin_logic.rb"
-      load File.open(plugin_logic)
+      load File.open(PLUGIN_LOGIC)
     end
   end
 end
