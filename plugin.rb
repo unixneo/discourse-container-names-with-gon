0c8800262aa22373fc258dc5b5d104058fb7828e
Tim Bass <neo@unix.com>
2020-11-22 11:54:49 +0700

    DEV:  Test new logic using ENV["DATA_NAME"] and 'df'

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: container-names-with-gon
 # about: plugin to add container names or ids from yml to backup page
-# version: 0.0.9.2
-# date: 12 Nov 2020
+# version: 0.0.9.3
+# date: 22s Nov 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-container-names-with-gon
 
@@ -13,22 +13,19 @@ require "gon"
 after_initialize do
   df = `df | grep shared`.split(" ")
   if df.size > 5
-    Gon.global.diskspace = "#{df[5]}: #{df[4]}"
+    Gon.global.diskspace = "#{df[0]}  #{df[4]}  #{df[5]}"
   else
-    Gon.global.diskspace = "TBD"
+    Gon.global.diskspace = "unknown"
   end
   if ENV["DATA_NAME"].present?
     container = ENV["DATA_NAME"].split("/")
     if container.size > 2
-      containers = "Containers: #{container[1].chomp}, #{container[2].chomp}"
       Gon.global.container_main = container[1].chomp
       Gon.global.container_data = container[2].chomp
     elsif container.size > 1
-      containers = "Containers: #{container[1].chomp}"
       Gon.global.container_main = container[1].chomp
       Gon.global.container_data = ""
     else
-      containers = "Containers: unknown, unknown"
       Gon.global.container_main = "unknown"
       Gon.global.container_data = "unknown"
     end
