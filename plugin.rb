fb957c1817253182e722b27b93f1d6700f4cfe5f
Tim Bass <neo@unix.com>
2020-12-05 19:48:17 +0700

    REFACTOR:  load to require.

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-container-names-with-gon
 # about: plugin to add container names or ids from yml to backup page
-# version: 0.1.42
-# date: 4 Dec 2020
+# version: 0.1.44
+# date: 5 Dec 2020
 # authors: Neo
 # url: https://github.com/unixneo/discourse-container-names-with-gon
 
@@ -12,10 +12,7 @@ require "gon"
 
 Gon.global.test = "HELLO GON"
 
-APP_ROOT = "#{Rails.root}/plugins/discourse-container-names-with-gon/app".freeze
-PLUGIN_NAME = "container-names-with-gon".freeze
-PLUGIN_LOGIC = "#{APP_ROOT}/lib/set_gon_info.rb".freeze
-load File.open(PLUGIN_LOGIC)
+require File.expand_path("../app/lib/set_gon_info.rb", __FILE__)
 
 after_initialize do
   GonLayoutChanges.add_gon_to_head
