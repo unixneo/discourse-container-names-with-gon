d699c343b4229d40fd7a80f491f3e85bdc80d2be
unixneo <neo@unix.com>
2021-01-02 16:39:24 +0700

    REFACTOR: limit to production ENV

--- plugin.rb
+++ plugin.rb
@@ -1,29 +1,29 @@
 # name: discourse-container-names-with-gon
 # about: plugin to add container names or ids from yml to backup page
-# version: 0.1.5
-# date: 27 Dec 2020
+# version: 0.1.6
+# date: 2 Jan 2021
 # authors: Neo
 # url: https://github.com/unixneo/discourse-container-names-with-gon
 
-register_asset "stylesheets/common/container-names.scss"
+if ENV["RAILS_ENV"] == "production"
+  register_asset "stylesheets/common/container-names.scss"
 
-gem "gon", "6.2.0"
-require "gon"
+  gem "gon", "6.2.0"
+  require "gon"
 
-Gon.global.test = "HELLO GON"
+  load File.expand_path("../app/lib/set_gon_info.rb", __FILE__)
 
-load File.expand_path("../app/lib/set_gon_info.rb", __FILE__)
+  after_initialize do
+    GonLayoutChanges.add_gon_to_head
+    GetContainerInfo.names
+    GetContainerInfo.diskspace
 
-after_initialize do
-  GonLayoutChanges.add_gon_to_head
-  GetContainerInfo.names
-  GetContainerInfo.diskspace
+    Admin::AdminController.class_eval do
+      before_action :do_info
 
-  Admin::AdminController.class_eval do
-    before_action :do_info
-
-    def do_info
-      GetContainerInfo.diskspace
+      def do_info
+        GetContainerInfo.diskspace
+      end
     end
   end
 end
