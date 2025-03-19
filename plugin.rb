8e88c74bfa346a82f675f7c8ece8ea4a2d409306
neounix <neo@unix.com>
2025-03-19 20:03:14 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-container-names-with-gon
 # about: plugin to add container names or ids from yml to backup page
-# version: 0.1.89
-# date: 3 April 2024
+# version: 0.2.0
+# date: 19 <arch 2025
 # authors: Neo
 # url: https://github.com/unixneo/discourse-container-names-with-gon
 
